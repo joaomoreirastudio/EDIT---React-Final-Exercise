a75ffb3612f318ee7d08b297d5c7a1de0ca1b6bd
João Moreira <joaomoreirastudio@gmail.com>
2024-08-22 18:21:48 +0100

    chore:folder update

---
+++ src/components/NavBar/NavBar.styled.ts
@@ -0,0 +1,64 @@
+import { FaChevronDown } from "react-icons/fa";
+import styled from "styled-components";
+const flex = (alignItems: string, justifyContent: string) => `
+  display: flex;
+  align-items: ${alignItems};
+  justify-content: ${justifyContent};
+`;
+
+export const NavbarContainer = styled.div`
+    ${flex("center", "space-between")}
+    height: 4rem;
+`;
+
+export const NavbarMenu = styled.div`
+    ${flex("center", "space-between")};
+    flex-direction: column;
+    @media screen and (min-width: 768px) {
+        gap: 1rem;
+        color: black;
+    }
+`;
+
+export const NavbarMenuDesktop = styled.div`
+    display: none;
+    @media screen and (min-width: 768px) {
+        display: block;
+    }
+`;
+
+export const NavBarLogo = styled.div`
+    width: 8rem;
+    padding: 0.5rem;
+`;
+
+export const NavBarLogoImg = styled.img`
+    width: 100%;
+`;
+
+export const NavBarList = styled.li`
+    ${flex("center", "space-between")};
+    @media screen and (min-width: 768px) {
+    }
+`;
+
+export const NavBarListContainer = styled.ul`
+    ${flex("center", "center")};
+    flex-direction: column;
+    gap: 1rem;
+    @media screen and (min-width: 768px) {
+        ${flex("center", "space-between")};
+        flex-direction: row;
+        gap: 1rem;
+    }
+`;
+
+export const HamburgerContainer = styled.div`
+    display: block; // Garante que esteja visível por padrão
+    position: absolute; // Posiciona o container de forma absoluta
+    right: 0.5rem;
+
+    @media screen and (min-width: 768px) {
+        display: none; // Esconde o container em telas maiores que 768px
+    }
+`;
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,71 @@
-import style from "../NavBar/NavBar.module.scss";
+import {
+    NavbarContainer,
+    NavbarMenu,
+    NavBarLogo,
+    NavBarLogoImg,
+    NavBarListContainer,
+    NavBarList,
+    HamburgerContainer,
+    NavbarMenuDesktop,
+} from "./NavBar.styled.js";
+import { Twirl as Hamburger } from "hamburger-react";
+import { useState } from "react";
+import Logo from "/src/assets/logo-react-site.svg";
+import { FaChevronDown } from "react-icons/fa6";
+
 const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleHamburgerClick = () => {
+        setMenuOpen(!menuOpen);
+    };
     return (
         <>
-            <nav className={style.navbar__container}>
-                <div className="navbar__logo">Logo</div>
-                <div className={style.navbar__menu}>hamburguer</div>
-            </nav>
+            <NavbarContainer>
+                <NavBarLogo>
+                    <NavBarLogoImg src={Logo} />
+                </NavBarLogo>
+                <NavbarMenuDesktop>
+                    <NavBarListContainer>
+                        <NavBarList>
+                            Seating <FaChevronDown />
+                        </NavBarList>
+                        <NavBarList>
+                            Living Room
+                            <FaChevronDown />
+                        </NavBarList>
+                        <NavBarList>
+                            Outdoor
+                            <FaChevronDown />
+                        </NavBarList>
+                        <NavBarList>
+                            Bedroom
+                            <FaChevronDown />
+                        </NavBarList>
+                        <NavBarList>
+                            Storage
+                            <FaChevronDown />
+                        </NavBarList>
+                    </NavBarListContainer>
+                </NavbarMenuDesktop>
+                <HamburgerContainer>
+                    <Hamburger
+                        toggled={menuOpen}
+                        toggle={handleHamburgerClick}
+                    />
+                </HamburgerContainer>
+            </NavbarContainer>
+            <NavbarMenu>
+                {menuOpen && (
+                    <NavBarListContainer>
+                        <NavBarList>Seating</NavBarList>
+                        <NavBarList>Living Room</NavBarList>
+                        <NavBarList>Outdoor</NavBarList>
+                        <NavBarList>Bedroom</NavBarList>
+                        <NavBarList>Storage</NavBarList>
+                    </NavBarListContainer>
+                )}
+            </NavbarMenu>
         </>
     );
 };
