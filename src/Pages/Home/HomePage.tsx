d568fe8eb40b458ae68b002dcc21559a1450b18d
João Moreira <joaomoreirastudio@gmail.com>
2024-09-26 19:51:55 +0100

    fix:shared-images api

--- src/Pages/Home/HomePage.tsx
+++ src/Pages/Home/HomePage.tsx
@@ -23,7 +23,7 @@ function HomePage() {
     const [categories, setCategories] = useState<Category[]>([]);
 
     useEffect(() => {
-        fetch("/public/api/product-category.json")
+        fetch("/api/product-category.json")
             .then((response) => response.json())
             .then((data) => setCategories(data))
             .catch((error) => console.error("Error fetching data:", error));
@@ -31,7 +31,7 @@ function HomePage() {
     const [sharedImg, SetSharedImg] = useState<SharedImg[]>([]);
 
     useEffect(() => {
-        fetch("/shared-images.json")
+        fetch("/api/shared-images.json")
             .then((response) => response.json())
             .then((data) => SetSharedImg(data))
             .catch((error) => console.error("Error fetching data:", error));
